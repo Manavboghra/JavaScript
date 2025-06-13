db20d2e8ad98a644ee77af46c611863accefdaeb
Manavboghra <153600297+Manavboghra@users.noreply.github.com>
2025-06-13 11:48:32 +0000

    datatype, array and objects

--- 01_Basics/datatype.js
+++ 01_Basics/datatype.js
@@ -1,17 +1,19 @@
 const arr = ["Apple", "dragon_fruit", "kivi"];
-console.log(typeof arr)
+// console.log(typeof arr)
 
 let userInformation = {
     name: "Tom",
     age: 22,
 }
-console.log(typeof userInformation)
+// console.log(typeof userInformation)
 
 let fun = function (){
-    console.log("hello World")
+    // console.log("hello World")
 }
 
- console.log(typeof fun)
+//  console.log(typeof fun)
 
 let bigNumber = 23983374n;
- console.log(typeof bigNumber)
+//  console.log(typeof bigNumber)
+
+
---
+++ 02_Basics/functions.js
@@ -0,0 +1,9 @@
+function fruit(){
+    console.log("Apple");
+    console.log("Litchi");
+    console.log("Kiwi");
+    console.log("Orange");
+    console.log("Mango");
+}
+
+fruit()
